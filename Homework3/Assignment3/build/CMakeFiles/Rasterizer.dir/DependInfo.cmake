c8b8d49302aacf4bfb4f7d2dd509c466c89460a6
lc06 <2546655793@qq.com>
2023-04-17 21:30:12 +0800

    recover Assignment3/build folder

---
+++ Homework3/Assignment3/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework3/Assignment3/Texture.cpp" "CMakeFiles/Rasterizer.dir/Texture.cpp.obj" "gcc" "CMakeFiles/Rasterizer.dir/Texture.cpp.obj.d"
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework3/Assignment3/Triangle.cpp" "CMakeFiles/Rasterizer.dir/Triangle.cpp.obj" "gcc" "CMakeFiles/Rasterizer.dir/Triangle.cpp.obj.d"
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework3/Assignment3/main.cpp" "CMakeFiles/Rasterizer.dir/main.cpp.obj" "gcc" "CMakeFiles/Rasterizer.dir/main.cpp.obj.d"
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework3/Assignment3/rasterizer.cpp" "CMakeFiles/Rasterizer.dir/rasterizer.cpp.obj" "gcc" "CMakeFiles/Rasterizer.dir/rasterizer.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Homework3/Assignment3/build/CMakeFiles/Rasterizer.dir/Texture.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/Rasterizer.dir/Texture.cpp.obj: \
+ D:\__Code__\Code_Repository\GAMES101_Homework\Homework3\Assignment3\Texture.cpp
