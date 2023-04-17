69a830afa05d2ea9d34551869ae7f8f08d88eb82
lc06 <2546655793@qq.com>
2023-04-17 21:23:39 +0800

    first commit

---
+++ Homework5/Assignment5/build/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework5/Assignment5/Renderer.cpp" "CMakeFiles/RayTracing.dir/Renderer.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Renderer.cpp.obj.d"
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework5/Assignment5/Scene.cpp" "CMakeFiles/RayTracing.dir/Scene.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Scene.cpp.obj.d"
+  "D:/__Code__/Code_Repository/GAMES101_Homework/Homework5/Assignment5/main.cpp" "CMakeFiles/RayTracing.dir/main.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ README.md
@@ -0,0 +1,33 @@
+# GAMES101 Homework
+Completed to Assignment 5...
+
+Lecture link: [GAMES101-现代计算机图形学入门-闫令琪](https://www.bilibili.com/video/BV1X7411F744)
+
+## Assignment1
+![Assignment1](Homework1/Assignment1/screenshots/Assignment1.png)
+
+## Assignment2
+![Assignment2](Homework2/Assignment2/screenshots/Assignment2.png)
+
+## Assignment3
+
+- phong
+![Assignment3](Homework3/Assignment3/build/phong.png)
+
+- texture
+![Assignment3](Homework3/Assignment3/build/texture.png)
+
+- normal
+![Assignment3](Homework3/Assignment3/build/normal.png)
+
+- bump
+![Assignment3](Homework3/Assignment3/build/bump.png)
+
+- displacement
+![Assignment3](Homework3/Assignment3/build/displacement.png)
+
+## Assignment4
+![Assignment4](Homework4/Assignment4/build/my_bezier_curve.png)
+
+## Assignment5
+![Assignment5](Homework5/Assignment5/build/Assignment5.png)
